a32a0ddf0357665651952d80f2b30f2dbac84721
swizz <emailswizz@gmail.com>
2026-02-09 23:55:01 -0500

    Initial Commit

---
+++ inspect/inspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  inspectApp.swift
+//  inspect
+//
+//  Created by Jack Smith on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct inspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
